01b8b3c19a143befb7f4013249d433c69c1f7707
Edo <editestar@gmail.com>
2026-06-13 10:33:58 +0200

    migliorato AdminSeeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -10,15 +10,23 @@ class AdminSeeder extends Seeder
 {
     public function run(): void
     {
-        $admin = User::firstOrCreate(
-            ['email' => 'admin@officinahub.local'],
-            [
-                'name' => 'Amministratore',
-                'password' => Hash::make('password'),
-                'email_verified_at' => now(),
-            ]
-        );
+        $users = [
+            ['email' => 'admin@admin.admin',                    'name' => 'Amministratore',  'role' => 'admin'],
+            ['email' => 'accettatore@accettatore.accettatore',  'name' => 'Accettatore',     'role' => 'accettatore'],
+            ['email' => 'meccanico@meccanico.meccanico',        'name' => 'Meccanico',       'role' => 'meccanico'],
+            ['email' => 'cassa@cassa.cassa',                    'name' => 'Cassa',           'role' => 'cassa'],
+        ];
 
-        $admin->assignRole('admin');
+        foreach ($users as $data) {
+            $user = User::firstOrCreate(
+                ['email' => $data['email']],
+                [
+                    'name' => $data['name'],
+                    'password' => Hash::make('admin'),
+                    'email_verified_at' => now(),
+                ]
+            );
+            $user->assignRole($data['role']);
+        }
     }
 }
